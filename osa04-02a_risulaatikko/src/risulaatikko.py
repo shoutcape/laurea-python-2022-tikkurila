d462f121e83056bccd874e6f89efc55fbe11947e
Shoutcape <74509593+shoutcape@users.noreply.github.com>
2023-05-05 21:07:47 +0300

    risulaatikko

---
+++ osa04-02a_risulaatikko/src/risulaatikko.py
@@ -0,0 +1,9 @@
+# kopioi edellisestä tehtävästä funktion viiva koodi tänne
+
+def risulaatikko(korkeus):
+    # täällä tulee kutsua funktiota viiva sopivilla parametreilla
+    viiva(10, "#")
+
+# funktiota kannattaa testata kutsumalla sitä täällä seuraavasti
+if __name__ == "__main__":
+    risulaatikko(5)
