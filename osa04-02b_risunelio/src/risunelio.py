fed9df2afdef64996ca4517af481c8b5d6165624
Shoutcape <74509593+shoutcape@users.noreply.github.com>
2023-05-05 21:10:37 +0300

    updeditti

---
+++ osa04-02b_risunelio/src/risunelio.py
@@ -0,0 +1,9 @@
+# kopioi edellisestä tehtävästä funktion viiva koodi tänne
+
+def risunelio(koko):
+    # täällä tulee kutsua funktiota viiva sopivilla parametreilla
+    viiva(4, "#")
+
+# funktiota kannattaa testata kutsumalla sitä täällä seuraavasti
+if __name__ == "__main__":
+    risunelio(5)
---
+++ osa04-02c_nelio/src/nelio.py
@@ -0,0 +1,9 @@
+# kopioi edellisestä tehtävästä funktion viiva koodi tänne
+
+def nelio(koko, merkki):
+    # täällä tulee kutsua funktiota viiva sopivilla parametreilla
+    viiva(4, "x")
+
+# funktiota kannattaa testata kutsumalla sitä täällä seuraavasti
+if __name__ == "__main__":
+    nelio(5, "x")
