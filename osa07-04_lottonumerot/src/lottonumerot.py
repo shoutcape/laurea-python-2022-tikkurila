4dac9599fe7d7140c0314fb01742aa7a26043b15
Shoutcape <74509593+shoutcape@users.noreply.github.com>
2023-05-22 17:15:41 +0300

    osio 7 alkua

--- osa07-04_lottonumerot/src/lottonumerot.py
+++ osa07-04_lottonumerot/src/lottonumerot.py
@@ -1,1 +1,11 @@
-# tee ratkaisu tänne
+from random import shuffle
+
+def lottonumerot(maara: int, alaraja: int, ylaraja: int):
+    numerot = list(range(alaraja,ylaraja+1))
+    shuffle(numerot)
+    lottonumerorivi = sorted(numerot[0:maara])
+    return  lottonumerorivi
+
+if __name__ == "__main__":
+    for numero in lottonumerot(7, 1, 40):
+        print(numero)
--- osa07-05_salasanan_arpoja_1/src/salasanan_arpoja.py
+++ osa07-05_salasanan_arpoja_1/src/salasanan_arpoja.py
@@ -1,1 +1,15 @@
-# tee ratkaisu tänne
+from random import choice
+from string import ascii_lowercase
+
+def luo_salasana(pituus: int):
+    salasana = ""
+    for i in range(pituus):
+        salasana += choice(ascii_lowercase)
+    
+    return salasana
+    
+        
+if __name__ == "__main__":
+  
+    for i in range (3):
+        print(luo_salasana(3))
